b2b9604e3b0d8c4480c9a5b74d8f8ee4b8bee2be
ananya2908 <290srivastavaananya@gmail.com>
2021-05-10 20:55:29 +0530

    Added function to print

--- src/Helper.java
+++ src/Helper.java
@@ -3,5 +3,10 @@ public class Helper {
     public static void main(String []args) {
         String a = "Arpit";
         System.out.println(a);
+        xyz();
+    }
+
+    public static void xyz() {
+        System.out.println("Anu");
     }
 }
